224972adef45290cdd69fd1853a6968b11b17e69
Elyza <elyzajanerelucio@gmail.com>
2023-04-28 07:37:39 +0800

    Create empty lists to store even and odd numbers

--- Odd_and_even.py
+++ Odd_and_even.py
@@ -5,6 +5,8 @@ numbers_list = numbers_file.read().split()
 # Close the file
 numbers_file.close()
 # Create two empty lists to store even and odd numbers
+even_numbers = []
+odd_numbers = []
 # Loop through the numbers list and append even and odd numbers to their respective lists
 # Write the even numbers to a file named "even.txt"
 # Write the odd numbers to a file named "odd.txt"
