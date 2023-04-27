14d895d3a70b80582b44f49d30d720723ef4eb5f
Elyza <elyzajanerelucio@gmail.com>
2023-04-28 07:34:30 +0800

    Read the numbers from the file, split them into a list and then close the file

--- Odd_and_even.py
+++ Odd_and_even.py
@@ -1,7 +1,9 @@
 # Open the file containing numbers
 numbers_file = open("numbers.txt", "r")
 # Read the numbers from the file and split them into a list
+numbers_list = numbers_file.read().split()
 # Close the file
+numbers_file.close()
 # Create two empty lists to store even and odd numbers
 # Loop through the numbers list and append even and odd numbers to their respective lists
 # Write the even numbers to a file named "even.txt"
