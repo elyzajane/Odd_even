36d15a04782235affeb13b41c4d8ba4e67bd1e25
Elyza <elyzajanerelucio@gmail.com>
2023-04-28 07:25:00 +0800

    Program outline

---
+++ Odd_and_even.py
@@ -0,0 +1,8 @@
+# Open the file containing numbers
+# Read the numbers from the file and split them into a list
+# Close the file
+# Create two empty lists to store even and odd numbers
+# Loop through the numbers list and append even and odd numbers to their respective lists
+# Write the even numbers to a file named "even.txt"
+# Write the odd numbers to a file named "odd.txt"
+# Print the output
